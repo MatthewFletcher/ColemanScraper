3fbcab39662690441b12aca56a2018ad9efe4cd7
Fletcher <bassmattf@gmail.com>
2019-03-04 14:56:36 -0600

    Added case statement for which class, also passed class name to python script

--- getslides.sh
+++ getslides.sh
@@ -1,15 +1,46 @@
 #!/bin/bash
 
 
+baseURL="http://www.cs.uah.edu/~rcoleman/"
 
 
-python3 linkscraper.py > links
+extra="/TodaysClass/"
 
-baseURL="http://www.cs.uah.edu/~rcoleman/CS307/TodaysClass/"
+pyextra="/TodaysClass/TodaysClass.html"
+
+PS3='Enter your choice of class >> '
+
+options=("CS221" "CS307")
+select opt in "${options[@]}"
+do
+  case $opt in
+    "CS221")
+      echo "CS221 chosen" 
+      class='CS221'
+      break
+      ;;
+    "CS307")
+      echo "CS307 chosen"
+      class='CS307'
+      break
+      ;;
+  esac
+done
+
+class="CS307"
+
+pyURL=$baseURL$class$pyextra
+
+echo python url passed: $pyURL
+
+python3 linkscraper.py $pyURL > links
+
+#UNCOMMENT TO AVOID DOWNLOADING FILES
+#exit 0
 
 n=1
 while read line; do
-  fullURL=$baseURL$line
+  fullURL=$baseURL$class$extra$line
   curl --silent -O $fullURL
   #echo $fullURL
   #n=$((n+1))
