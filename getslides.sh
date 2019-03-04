b1cc6627d5ba6e38a67d9eece47920d46942206c
Fletcher <bassmattf@gmail.com>
2019-03-04 14:07:42 -0600

    First commit

---
+++ getslides.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+
+
+python3 .getlinks.py > links
+
+baseURL="http://www.cs.uah.edu/~rcoleman/CS307/TodaysClass/"
+
+n=1
+while read line; do
+  fullURL=$baseURL$line
+  curl --silent -O $fullURL
+  #echo $fullURL
+  #n=$((n+1))
+done < links
+
+
+convert $(ls *.jpg) slides.pdf 2>/dev/null
+
+rm links
+rm *.jpg
