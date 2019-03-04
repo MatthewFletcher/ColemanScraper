0f3c2a8605db263af51e6f64d5d9b699f04a3454
Fletcher <bassmattf@gmail.com>
2019-03-04 14:56:09 -0600

    linkscraper now gets url to scrape from command line arg instead of hard coding

--- linkscraper.py
+++ linkscraper.py
@@ -3,22 +3,21 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import requests
+import sys
 
-
-target="http://www.cs.uah.edu/~rcoleman/CS307/TodaysClass/TodaysClass.html"
-
+#target="http://www.cs.uah.edu/~rcoleman/CS307/TodaysClass/TodaysClass.html"
+target=sys.argv[1]
 
 #https://stackoverflow.com/questions/3969726/attributeerror-module-object-has-no-attribute-urlopen
+
+#print("Target URL: ", target)
 with urllib.request.urlopen(target) as url:
     s = url.read()
 
 
 soup = BeautifulSoup(s,'html.parser') 
 
-images = []
 
 for img in soup.findAll('img'):
     imgsrc = img.get('src')
-    #images.append(img.get('src'))
     print(imgsrc)
-#print(images)
